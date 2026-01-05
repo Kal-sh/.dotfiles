5ae6976b1b7f04dd6acfe12028f1a84a201af7fa
kal-sh <kalecherinet@gmail.com>
2026-01-05 19:36:52 +0300

    removed most of vscodium installation folder and only kept the config files

--- .config/nvim/lua/plugins/color_scheme.lua
+++ .config/nvim/lua/plugins/color_scheme.lua
@@ -8,3 +8,25 @@ return {
     },
   },
 }
+
+--[[
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
+]]
